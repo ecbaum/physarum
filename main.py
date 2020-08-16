ba6a9e7ed8e51d613aa25f96e9921961524fecd6
Jerkus <69682563+Jerkus@users.noreply.github.com>
2020-08-16 14:58:42 +0200

    Entropy analysis added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from maps import DataMap, TrailMap
 import matplotlib.pyplot as plt
+import numpy as np
+import skimage.measure
 
 sz = (100, 133)
 cells = 400
+simulation_length = 2000
 
 dm = DataMap(sz)
 tm = TrailMap(sz)
@@ -14,11 +17,22 @@ fig, (ax1, ax2) = plt.subplots(1, 2)
 im1 = ax1.imshow(dm.grid)
 im2 = ax2.imshow(tm.grid)
 
-for i in range(1000):
+entropy = np.zeros(simulation_length)
+
+for i in range(simulation_length):
     dm.cell_operation(tm)
     tm.diffuse(dm)
     im1.set_array(dm.grid)
     im2.set_array(tm.grid)
+
+    entropy[i] = skimage.measure.shannon_entropy(dm.grid)
+
     plt.pause(0.01)
 
 plt.show()
+
+plt.figure()
+plt.plot(entropy)
+plt.title("Entropy of system over time")
+plt.ylabel("Shannon entropy")
+plt.xlabel("Iteration")
